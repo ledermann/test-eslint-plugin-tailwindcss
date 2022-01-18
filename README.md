98e58ea322146533445ba80a83a955c72e13b50c
Georg Ledermann <georg@ledermann.dev>
2022-01-18 09:41:32 +0100

    :tada: Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Testing eslint-plugin-tailwindcss
+
+See https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/85
+
+```
+npm i
+npm run eslint
+```
---
+++ test.js
@@ -0,0 +1,8 @@
+<html>
+  <div class="px-10 px-20">...</div>
+
+  <div class=" lg:p-8 rounded sm:rounded-lg lg:rounded-2xl p-4 sm:p-6">...</div>
+</html>
+
+const foo=1;
+alert(foo);
